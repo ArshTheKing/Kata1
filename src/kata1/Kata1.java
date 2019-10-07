746112fb0e18d4929e9fb98f30ddf1507f25e03f
Azael <Azael@CrescentRose>
2019-10-07 11:12:29 +0100

    Version 1 ( with Date deprecated)

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,11 @@
+package kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        System.out.println(new Person("Azael", new Date(99,7,20)).toString());
+    }
+    
+}
---
+++ src/kata1/Person.java
@@ -0,0 +1,29 @@
+package kata1;
+
+import java.util.Date;
+
+class Person {
+    private final String Name;
+    private final Date birthdate;
+    final long MS_YEARS=365*24*60*60*1000;
+    public Person(String Name, Date birthdate) {
+        this.Name = Name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "Name=" + Name + ", birthdate=" + birthdate.toString() + ", age=" +this.getAge()+'}';
+    }
+    public int getAge(){
+        return (int) ((new Date().getTime()-birthdate.getTime())/MS_YEARS);
+    }
+}
